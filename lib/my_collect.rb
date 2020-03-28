3296833442b8c2edfb7c37b26725f2c9f1d25c01
Kevin Mathias <komathias1@gmail.com>
2020-03-28 03:34:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,13 @@
 
 def my_collect(collection)
+  if collection == []
+  else
   i = 0
   collect = []
   while i < array.length
     collect << yield(array[i])
     i += 1
   end
+end
   collect
 end
